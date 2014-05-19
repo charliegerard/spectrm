54d31590f948f66a07b3c37ad5e18ae03e52229e
Charlie <annecharlotte.gerard@gmail.com>
2014-05-19 18:13:05 +1000

    Deleted JS

--- app/assets/javascripts/settings.js
+++
@@ -1,7 +0,0 @@
-// var settings = function(){
-// 	$('span.button.save-as').click(function(event){
-// 		event.preventDefault();
-// 		console.log("clicked on new");
-// 		console.log(presetName)
-// 	});
-// }
