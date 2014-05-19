39ad7998ba1ec262e6af92a6d7277e0b36eb69c9
Charlie <annecharlotte.gerard@gmail.com>
2014-05-19 18:23:34 +1000

    all js files

---
+++ app/assets/javascripts/settings.js
@@ -0,0 +1,7 @@
+// var settings = function(){
+// 	$('span.button.save-as').click(function(event){
+// 		event.preventDefault();
+// 		console.log("clicked on new");
+// 		console.log(presetName)
+// 	});
+// }
