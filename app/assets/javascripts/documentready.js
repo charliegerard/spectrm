36974d2c694bf391d051d1fab24277a9d73a7d08
Charlie <annecharlotte.gerard@gmail.com>
2014-05-23 16:01:35 +1000

    new comments

--- app/assets/javascripts/documentready.js
+++ app/assets/javascripts/documentready.js
@@ -1,18 +1,25 @@
-var soundcloundOn;
 
 $(document).ready(function(){
-	var soundcloundOn;
+	//Calls the cube as first shape to be displayed.
 	cube();
 
 	$('#microphone').click(function(){
+		/*Disconnects the source of the previous option selected. Without this, if 
+		you select to play the song before checking with the microphone, the analyser will 
+		be doubled up and the shape will move according to the song AND the microphone.*/
+
 		window.source && source.disconnect(0);
 		window.microphoneOn = true;
 
+		/*If we used the Souncloud player before activating the microphone, disconnect the analyser and
+		the source to avoid doubling up the data making the shape move. */
 		if(SoundCloud.analyser){
 			SoundCloud.soundcloudOn = false;
 			SoundCloud.analyser.disconnect();
 			SoundCloud.source.disconnect();
 		}
+
+		//Asks to activate the microphone and calls the function gotStream();
 		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;	
 		navigator.getUserMedia( {audio:true}, gotStream );
 	});
@@ -33,25 +40,29 @@ $(document).ready(function(){
 	$('#form').on('submit', function() {
 		analyser = null;
 		source.disconnect(0)
-		//soundcloudOn = true;
     });
 
+	//Calls the cube function when clicking on the cube in the menu.
 	$('#cube').click(function(){
 		cube();
 	});
 
+	//Same with landscape. A the moment, this option is not in the menu anymore.
 	$('#landscape').click(function(){
 		landscape();
 	});
 
+	//Same with city.
 	$('#city').click(function(){
 		city();
 	});
 
+	//Same with sphere
 	$('#sphere').click(function(){
 		sphere();
 	});
 
+	//Calls the Leap Motion function that only works with the cube at the moment.
 	$('#leap').click(function(){
 		leap();
 	});
@@ -78,6 +89,7 @@ $(document).ready(function(){
 		$('#sign-in-form').fadeIn();
 	});
 
+	//Handling the login with Ajax so it doesn't refresh the page.
 	$('.signInButton').click(function(event){
 		event.preventDefault();
 
